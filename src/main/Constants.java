bdc0b3131882f65501468c2bf506ac0472f0d04f
Paul Graham <p@ul.ms>
2019-11-05 21:15:35 +0000

    fixed tiny issue

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
     // Window constants
     public static final String NAME = "./Space.trash";
     public static final int WIDTH = 800;
-    public static final int HEIGHT = 800;
+    public static final int HEIGHT = 600;
 
     // GUI constants
     public static final int EDITOR_WIDTH = 400;
